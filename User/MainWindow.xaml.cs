6cedf8ae0328b605689f3c4099b9b86df9e9e8de
ACT <Solovjev.artem2003@yandex.ru>
2025-03-07 14:35:33 +0300

    Добавьте файлы проекта.

---
+++ User/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace User
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "act.ico");
+        }
+    }
+}
